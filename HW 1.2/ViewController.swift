ebb90642b04b499483e309fadb0393983f2023e8
Yuri <smart.money96@gmail.com>
2020-08-19 14:44:57 +0500

    Initial Commit

---
+++ HW 1.2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW 1.2
+//
+//  Created by Яна Варфоломеева on 19.08.2020.
+//  Copyright © 2020 Grigorev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
